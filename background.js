d4d3e482da6a18dea21620a9d94f220bce6a85b3
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-20 15:49:50 -0400

    overall correction and update

--- background.js
+++ background.js
@@ -6,24 +6,6 @@ chrome.tabs.onCreated.addListener(function(tab) {
     });
   });
   
-  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      if (tabs.length <= 10) {
-        chrome.browserAction.setBadgeText({text: ""});
-      }
-    });
-  });
-  
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      if (tabs.length > 10) {
-        if (changeInfo.url !== undefined) {
-          chrome.tabs.remove(tab.id);
-        }
-      }
-    });
-  });
-  
   chrome.tabs.query({currentWindow: true}, function(tabs) {
     if (tabs.length > 10) {
       chrome.browserAction.setBadgeText({text: "10+"});
@@ -31,4 +13,5 @@ chrome.tabs.onCreated.addListener(function(tab) {
       chrome.browserAction.setBadgeText({text: ""});
     }
   });
+  
   
--- popup.js
+++ popup.js
@@ -1,10 +1,23 @@
+// Get the tab count and update the UI
 function updateTabCount() {
     chrome.tabs.query({ currentWindow: true }, function (tabs) {
       var tabCount = tabs.length;
       var tabCountElement = document.getElementById("tabCount");
       tabCountElement.innerText = tabCount;
-      if (tabCount > 10) {
-        chrome.tabs.remove(tabs[tabCount - 1].id);
+      if (tabCount >= 10) {
+        alert("You have reached the maximum limit of 10 tabs per window.");
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+          chrome.tabs.remove(tabs[0].id);
+        });
       }
     });
   }
+  
+  // Update the tab count when the popup is opened
+  document.addEventListener("DOMContentLoaded", function () {
+    updateTabCount();
+  });
+  
+  // Update the tab count when a tab is created or removed
+  chrome.tabs.onCreated.addListener(updateTabCount);
+  chrome.tabs.onRemoved.addListener(updateTabCount);
