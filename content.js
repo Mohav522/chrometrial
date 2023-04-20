3a5794bb08614a6d395f815b27d1a67014eef26a
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-20 13:34:40 -0400

    content,js new

--- content.js
+++ content.js
@@ -1,25 +1,24 @@
 let tabCount = 0;
 
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.command === "getTabCount") {
-    sendResponse(tabCount);
-  } else if (msg.command === "incrementTabCount") {
-    tabCount++;
-  } else if (msg.command === "decrementTabCount") {
-    tabCount--;
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.message === "getTabCount") {
+    sendResponse({count: tabCount});
   }
 });
 
-chrome.tabs.onCreated.addListener(function (tab) {
-  chrome.tabs.query({ currentWindow: true }, function (tabs) {
+chrome.tabs.onCreated.addListener(function(tab) {
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
     if (tabs.length > 10) {
       chrome.tabs.remove(tab.id);
-    } else {
-      chrome.runtime.sendMessage({ command: "incrementTabCount" });
+    }
+    else {
+      tabCount = tabs.length;
     }
   });
 });
 
-chrome.tabs.onRemoved.addListener(function (tab) {
-  chrome.runtime.sendMessage({ command: "decrementTabCount" });
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    tabCount = tabs.length;
+  });
 });
