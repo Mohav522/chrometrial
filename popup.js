f43ba26d88d2d4f0153a05c2831e3cb9f80dfb46
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-20 22:24:20 -0400

    add audio

--- popup.js
+++ popup.js
@@ -26,62 +26,27 @@ chrome.tabs.query({}, function(tabs) {
   
   chrome.tabs.query({}, function(tabs) {
     var count = tabs.length;
-  
+    
     const MAX_TABS = 10;
-  
+    var audio = new Audio('bell.mp3');
+    
     chrome.tabs.onCreated.addListener(function(tab) {
-  
       chrome.tabs.query({currentWindow: true}, function(tabs) {
-  
         if (tabs.length > MAX_TABS) {
-  
           chrome.tabs.remove(tab.id);
-  
           chrome.notifications.create({
             type: "basic",
             title: "Tab Limit Exceeded",
             message: "You have reached the maximum number of tabs in this window.",
             iconUrl: "icon-48.png"
           });
+          if (tabs.length == MAX_TABS + 1) {
+            audio.play();
+          }
         }
       });
     });
   
     document.getElementById("tab-counter").textContent = count;
-  });
-  
-  chrome.tabs.query({}, function (tabs) {
-    var count = tabs.length;
-    var MAX_TABS = 10;
-  
-    chrome.tabs.onCreated.addListener(function (tab) {
-      chrome.tabs.query({ currentWindow: true }, function (tabs) {
-        if (tabs.length > MAX_TABS) {
-          chrome.tabs.remove(tab.id);
-  
-          chrome.notifications.create({
-            type: "basic",
-            title: "Tab Limit Exceeded",
-            message: "You have reached the maximum number of tabs in this window.",
-            iconUrl: "icon-48.png",
-          });
-        }
-      });
-    });
-  
-    document.getElementById("tab-counter").textContent = count;
-  
-    var progressBar = document.getElementById("tab-counter-bar");
-  
-    if (count >= 0 && count <= 7) {
-      progressBar.style.backgroundColor = "#7fff00";
-      progressBar.style.width = count * 10 + "%";
-    } else if (count > 7 && count <= 10) {
-      progressBar.style.backgroundColor = "#ff8c00";
-      progressBar.style.width = count * 10 + "%";
-    } else {
-      progressBar.style.backgroundColor = "#ff0000";
-      progressBar.style.width = "100%";
-    }
   });
   
