29aff3fb30734c9ffcc18727eb9f6135315c9ce4
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-20 21:20:47 -0400

    progress bar digital

--- popup.js
+++ popup.js
@@ -24,22 +24,34 @@ chrome.tabs.query({}, function(tabs) {
     document.getElementById("Tab Limiter").textContent = count;
   });
   
-  // Get the total number of tabs
-chrome.tabs.query({}, function(tabs) {
-    const totalCount = tabs.length;
-    
-    // Set the maximum number of tabs
+  chrome.tabs.query({}, function(tabs) {
+    var count = tabs.length;
     const MAX_TABS = 10;
   
-    // Calculate the percentage of tabs used
-    const percentage = Math.min((totalCount / MAX_TABS) * 100, 100);
+    chrome.tabs.onCreated.addListener(function(tab) {
+      chrome.tabs.query({currentWindow: true}, function(tabs) {
+        if (tabs.length > MAX_TABS) {
+          chrome.tabs.remove(tab.id);
+          chrome.notifications.create({
+            type: "basic",
+            title: "Tab Limit Exceeded",
+            message: "You have reached the maximum number of tabs in this window.",
+            iconUrl: "icon-48.png"
+          });
+        } else {
+          updateProgressBar(tabs.length);
+        }
+      });
+    });
   
-    // Get the progress bar element
-    const progressBar = document.getElementById("progress-bar");
+    function updateProgressBar(tabCount) {
+      var progressBar = document.querySelector('.progress-bar');
+      var tabNumber = document.querySelector('.tab-number span');
+      var progressPercent = (tabCount / MAX_TABS) * 100;
+      progressBar.style.width = progressPercent + '%';
+      tabNumber.textContent = tabCount;
+    }
   
-    // Set the progress bar width
-    progressBar.style.width = `${percentage}%`;
-  
-    // Set the progress bar text
-    progressBar.textContent = `${totalCount} / ${MAX_TABS}`;
+    updateProgressBar(count);
   });
+  
