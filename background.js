74116e494024ae93e7e740f28d081889f339529d
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-21 14:12:38 -0400

    all files updated as per Manifest v3

--- background.js
+++ background.js
@@ -9,18 +9,17 @@ chrome.tabs.onCreated.addListener(function(tab) {
   chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
     chrome.tabs.query({currentWindow: true}, function(tabs) {
       if (tabs.length > 10) {
-        chrome.browserAction.setBadgeText({text: "10+"});
+        chrome.action.setBadgeText({text: "10+"});
       } else {
-        chrome.browserAction.setBadgeText({text: ""});
+        chrome.action.setBadgeText({text: ""});
       }
     });
   });
   
   chrome.tabs.query({currentWindow: true}, function(tabs) {
     if (tabs.length > 10) {
-      chrome.browserAction.setBadgeText({text: "10+"});
+      chrome.action.setBadgeText({text: "10+"});
     } else {
-      chrome.browserAction.setBadgeText({text: ""});
+      chrome.action.setBadgeText({text: ""});
     }
   });
-  
