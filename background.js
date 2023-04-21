3af2ac5af4a89efe3234891edeb6b1290f0057e5
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-21 14:27:22 -0400

    revert to v2

--- background.js
+++ background.js
@@ -1,25 +1,25 @@
 chrome.tabs.onCreated.addListener(function(tab) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      if (tabs.length > 10) {
-        chrome.tabs.remove(tab.id);
-      }
-    });
-  });
-  
-  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      if (tabs.length > 10) {
-        chrome.action.setBadgeText({text: "10+"});
-      } else {
-        chrome.action.setBadgeText({text: ""});
-      }
-    });
-  });
-  
   chrome.tabs.query({currentWindow: true}, function(tabs) {
     if (tabs.length > 10) {
-      chrome.action.setBadgeText({text: "10+"});
-    } else {
-      chrome.action.setBadgeText({text: ""});
+      chrome.tabs.remove(tab.id);
     }
   });
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    if (tabs.length > 10) {
+      chrome.browserAction.setBadgeText({text: "10+"});
+    } else {
+      chrome.browserAction.setBadgeText({text: ""});
+    }
+  });
+});
+
+chrome.tabs.query({currentWindow: true}, function(tabs) {
+  if (tabs.length > 10) {
+    chrome.browserAction.setBadgeText({text: "10+"});
+  } else {
+    chrome.browserAction.setBadgeText({text: ""});
+  }
+});
--- popup.js
+++ popup.js
@@ -1,28 +1,66 @@
 chrome.tabs.query({}, function(tabs) {
-    var count = tabs.length;
+  var count = tabs.length;
+
+  const MAX_TABS = 10;
+
+  chrome.tabs.onCreated.addListener(function(tab) {
+
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+
+      if (tabs.length > MAX_TABS) {
+
+        chrome.tabs.remove(tab.id);
+
+        chrome.notifications.create({
+          type: "basic",
+          title: "Tab Limit Exceeded",
+          message: "You have reached the maximum number of tabs in this window.",
+          iconUrl: "icon-48.png"
+        });
+      }
+    });
+  });
+
+  document.getElementById("Tab Limiter").textContent = count;
+});
+
   
+  chrome.tabs.query({}, function(tabs) {
+    var count = tabs.length;
     const MAX_TABS = 10;
+    var images = document.getElementById("tab-images");
   
     chrome.tabs.onCreated.addListener(function(tab) {
-  
       chrome.tabs.query({currentWindow: true}, function(tabs) {
-  
         if (tabs.length > MAX_TABS) {
-  
           chrome.tabs.remove(tab.id);
-  
           chrome.notifications.create({
             type: "basic",
             title: "Tab Limit Exceeded",
             message: "You have reached the maximum number of tabs in this window.",
             iconUrl: "icon-48.png"
           });
+        } else {
+          // Remove existing images
+          while (images.firstChild) {
+            images.removeChild(images.firstChild);
+          }
+  
+          // Add image for current tab count
+          var img = document.createElement("img");
+          img.src = "tab" + tabs.length + ".png";
+          images.appendChild(img);
         }
       });
     });
   
-    document.getElementById("Tab Limiter").textContent = count;
+    // Add initial image(s)
+    while (images.firstChild) {
+      images.removeChild(images.firstChild);
+    }
+    var img = document.createElement("img");
+    img.src = "tab" + count + ".png";
+    images.appendChild(img);
   });
-  
   
   
