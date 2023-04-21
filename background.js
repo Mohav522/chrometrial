bdb9707e966a000a9b7ebf7f9ac2dd42af42a499
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-21 13:48:45 -0400

    overall version update

--- background.js
+++ background.js
@@ -1,26 +1,24 @@
-chrome.tabs.onCreated.addListener(function(tab) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      if (tabs.length > 10) {
-        chrome.tabs.remove(tab.id);
-      }
-    });
-  });
+// Set the maximum number of allowed tabs
+const MAX_TABS = 10;
+
+// Listen for the browser action being clicked
+chrome.action.onClicked.addListener(async (tab) => {
+  // Get all the tabs in the current window
+  const tabs = await chrome.tabs.query({ currentWindow: true });
   
-  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      if (tabs.length > 10) {
-        chrome.browserAction.setBadgeText({text: "10+"});
-      } else {
-        chrome.browserAction.setBadgeText({text: ""});
-      }
-    });
-  });
+  // If the number of tabs is less than the maximum allowed, open a new tab
+  if (tabs.length < MAX_TABS) {
+    chrome.tabs.create({});
+  }
+});
+
+// Listen for tab creation events
+chrome.tabs.onCreated.addListener(async (tab) => {
+  // Get all the tabs in the current window
+  const tabs = await chrome.tabs.query({ currentWindow: true });
   
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    if (tabs.length > 10) {
-      chrome.browserAction.setBadgeText({text: "10+"});
-    } else {
-      chrome.browserAction.setBadgeText({text: ""});
-    }
-  });
-  
+  // If the number of tabs is greater than the maximum allowed, close the newly created tab
+  if (tabs.length > MAX_TABS) {
+    chrome.tabs.remove(tab.id);
+  }
+});
