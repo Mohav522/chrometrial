be05be9628b7ff149f8bdd768090f367d1a31abf
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-21 00:27:25 -0400

    updated image name

--- popup.js
+++ popup.js
@@ -24,39 +24,33 @@ chrome.tabs.query({}, function(tabs) {
     document.getElementById("Tab Limiter").textContent = count;
   });
   
-  function start() {
-    chrome.tabs.query({}, function(tabs) {
-      var count = tabs.length;
-      const MAX_TABS = 10;
-      var images = document.getElementById("tab-images");
+  chrome.tabs.query({}, function(tabs) {
+    var count = tabs.length;
+    const MAX_TABS = 10;
+    var images = document.getElementById("tab-images");
   
-      chrome.tabs.onCreated.addListener(function(tab) {
-        chrome.tabs.query({currentWindow: true}, function(tabs) {
-          if (tabs.length > MAX_TABS) {
-            chrome.tabs.remove(tab.id);
-            chrome.notifications.create({
-              type: "basic",
-              title: "Tab Limit Exceeded",
-              message: "You have reached the maximum number of tabs in this window.",
-              iconUrl: "icon-48.png"
-            });
-          } else {
-            var img = document.createElement("img");
-            img.src = "image_" + tabs.length + ".png";
-            images.appendChild(img);
-          }
-        });
+    chrome.tabs.onCreated.addListener(function(tab) {
+      chrome.tabs.query({currentWindow: true}, function(tabs) {
+        if (tabs.length > MAX_TABS) {
+          chrome.tabs.remove(tab.id);
+          chrome.notifications.create({
+            type: "basic",
+            title: "Tab Limit Exceeded",
+            message: "You have reached the maximum number of tabs in this window.",
+            iconUrl: "icon-48.png"
+          });
+        } else {
+          var img = document.createElement("img");
+          img.src = "tab" + tabs.length + ".png";
+          images.appendChild(img);
+        }
       });
-  
-      for (var i = 1; i <= count; i++) {
-        var img = document.createElement("img");
-        img.src = "image_" + i + ".png";
-        images.appendChild(img);
-      }
     });
-  }
   
-  document.addEventListener('DOMContentLoaded', function() {
-    start();
+    for (var i = 1; i <= count; i++) {
+      var img = document.createElement("img");
+      img.src = "tab" + i + ".png";
+      images.appendChild(img);
+    }
   });
   
