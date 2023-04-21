74812cc66614773f1a6a876cf8c1eb834bbbd051
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-21 00:16:13 -0400

    trial image replacement

--- popup.js
+++ popup.js
@@ -24,29 +24,39 @@ chrome.tabs.query({}, function(tabs) {
     document.getElementById("Tab Limiter").textContent = count;
   });
   
-  chrome.tabs.query({}, function(tabs) {
-    var count = tabs.length;
-    
-    const MAX_TABS = 10;
-    var audio = new Audio('bell.mp3');
-    
-    chrome.tabs.onCreated.addListener(function(tab) {
-      chrome.tabs.query({currentWindow: true}, function(tabs) {
-        if (tabs.length > MAX_TABS) {
-          chrome.tabs.remove(tab.id);
-          chrome.notifications.create({
-            type: "basic",
-            title: "Tab Limit Exceeded",
-            message: "You have reached the maximum number of tabs in this window.",
-            iconUrl: "icon-48.png"
-          });
-          if (tabs.length == MAX_TABS + 1) {
-            audio.play();
-          }
-        }
-      });
-    });
+  function start() {
+    chrome.tabs.query({}, function(tabs) {
+      var count = tabs.length;
+      const MAX_TABS = 10;
+      var images = document.getElementById("tab-images");
   
-    document.getElementById("tab-counter").textContent = count;
+      chrome.tabs.onCreated.addListener(function(tab) {
+        chrome.tabs.query({currentWindow: true}, function(tabs) {
+          if (tabs.length > MAX_TABS) {
+            chrome.tabs.remove(tab.id);
+            chrome.notifications.create({
+              type: "basic",
+              title: "Tab Limit Exceeded",
+              message: "You have reached the maximum number of tabs in this window.",
+              iconUrl: "icon-48.png"
+            });
+          } else {
+            var img = document.createElement("img");
+            img.src = "image_" + tabs.length + ".png";
+            images.appendChild(img);
+          }
+        });
+      });
+  
+      for (var i = 1; i <= count; i++) {
+        var img = document.createElement("img");
+        img.src = "image_" + i + ".png";
+        images.appendChild(img);
+      }
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    start();
   });
   
