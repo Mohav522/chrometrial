c1062b7bff095ffda0b7d7576af31ebd86c9091d
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-23 13:48:37 -0400

    icon update + notifications

--- background.js
+++ background.js
@@ -1,41 +1,74 @@
-let tabCount = 0;
-
-function updateBadge(tabCount) {
-  const badgeText = tabCount > 9 ? "10" : tabCount.toString();
-  chrome.browserAction.setBadgeText({ text: badgeText });
-  
-  let badgeColor = "";
-  if (tabCount >= 1 && tabCount <= 5) {
-    badgeColor = "#00FF00"; // green
-  } else if (tabCount >= 6 && tabCount <= 9) {
-    badgeColor = "#FFFF00"; // yellow
-  } else if (tabCount >= 10 && tabCount <= 10) {
-    badgeColor = "#FF0000"; // red
-  }
-  
-  chrome.browserAction.setBadgeBackgroundColor({ color: badgeColor });
-}
-
-function handleTabCount(tabs) {
-  tabCount = tabs.length;
-  
-  if (tabCount > 10) {
-    chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-      if (tabs[0]) {
-        chrome.tabs.remove(tabs[0].id);
-      }
+function showNotification() {
+    chrome.notifications.create({
+      type: "basic",
+      title: "Tab Limit Exceeded",
+      message: "Stop right here, there won't be an 11th!",
+      iconUrl: "icon.png"
     });
   }
   
-  updateBadge(tabCount);
-}
-
-chrome.tabs.onCreated.addListener(function(tab) {
-  chrome.tabs.query({ currentWindow: true }, handleTabCount);
-});
-
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-  chrome.tabs.query({ currentWindow: true }, handleTabCount);
-});
-
-chrome.tabs.query({ currentWindow: true }, handleTabCount);
+  chrome.tabs.onCreated.addListener(function(tab) {
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+      const tabCount = tabs.length;
+      if (tabCount > 10) {
+        chrome.tabs.remove(tab.id);
+        showNotification();
+      }
+    });
+  });
+  
+  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+      const tabCount = tabs.length;
+      if (tabCount > 10) {
+        showNotification();
+      }
+    });
+  });
+  
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    const tabCount = tabs.length;
+    if (tabCount > 10) {
+      showNotification();
+    }
+  });
+  
+  function updateBadge(tabCount) {
+    let color = "";
+    if (tabCount >= 1 && tabCount <= 5) {
+      color = "green";
+    } else if (tabCount >= 6 && tabCount <= 8) {
+      color = "yellow";
+    } else if (tabCount >= 9 && tabCount <= 10) {
+      color = "red";
+    }
+    const badgeText = tabCount > 9 ? "10+" : tabCount.toString();
+    chrome.browserAction.setBadgeText({text: badgeText});
+    chrome.browserAction.setBadgeBackgroundColor({color: color});
+  }
+  
+  chrome.tabs.onCreated.addListener(function(tab) {
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+      const tabCount = tabs.length;
+      if (tabCount > 10) {
+        chrome.tabs.remove(tab.id);
+        updateBadge(tabCount);
+      } else {
+        updateBadge(tabCount);
+      }
+    });
+  });
+  
+  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+      const tabCount = tabs.length;
+      updateBadge(tabCount);
+    });
+  });
+  
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    const tabCount = tabs.length;
+    updateBadge(tabCount);
+  });
+  
+  
