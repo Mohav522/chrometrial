8167c44b03df2866ef18866b63d4b1a1885be222
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-23 12:35:38 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,58 +1,41 @@
-var tabCount = 0;
+let tabCount = 0;
 
 function updateBadge(tabCount) {
-  var badgeText = "";
-  var badgeColor = "";
-  if (tabCount > 0 && tabCount <= 5) {
-    badgeText = tabCount.toString();
-    badgeColor = "#00b300"; // green
-  } else if (tabCount > 5 && tabCount <= 8) {
-    badgeText = tabCount.toString();
-    badgeColor = "#ffff4d"; // yellow
-  } else if (tabCount > 8 && tabCount <= 10) {
-    badgeText = tabCount.toString();
-    badgeColor = "#ff0000"; // red
-  } else if (tabCount > 10) {
-    badgeText = "10+";
-    badgeColor = "#ff0000"; // red
+  const badgeText = tabCount > 9 ? "10" : tabCount.toString();
+  chrome.browserAction.setBadgeText({ text: badgeText });
+  
+  let badgeColor = "";
+  if (tabCount >= 1 && tabCount <= 5) {
+    badgeColor = "#00FF00"; // green
+  } else if (tabCount >= 6 && tabCount <= 9) {
+    badgeColor = "#FFFF00"; // yellow
+  } else if (tabCount >= 10 && tabCount <= 10) {
+    badgeColor = "#FF0000"; // red
   }
-  chrome.browserAction.setBadgeText({text: badgeText});
-  chrome.browserAction.setBadgeBackgroundColor({color: badgeColor});
+  
+  chrome.browserAction.setBadgeBackgroundColor({ color: badgeColor });
 }
 
-function showAlert() {
-  var opt = {
-    type: "basic",
-    title: "Tab limit exceeded",
-    message: "Stop right here, there won't be an 11th!",
-    iconUrl: "icon.png"
-  };
-  chrome.notifications.create(opt);
+function handleTabCount(tabs) {
+  tabCount = tabs.length;
+  
+  if (tabCount > 10) {
+    chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+      if (tabs[0]) {
+        chrome.tabs.remove(tabs[0].id);
+      }
+    });
+  }
+  
+  updateBadge(tabCount);
 }
 
 chrome.tabs.onCreated.addListener(function(tab) {
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    const tabCount = tabs.length;
-    if (tabCount > 10) {
-      chrome.tabs.remove(tab.id);
-      showAlert();
-    }
-    updateBadge(tabCount);
-  });
+  chrome.tabs.query({ currentWindow: true }, handleTabCount);
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    const tabCount = tabs.length;
-    updateBadge(tabCount);
-  });
+  chrome.tabs.query({ currentWindow: true }, handleTabCount);
 });
 
-chrome.tabs.query({currentWindow: true}, function(tabs) {
-  const tabCount = tabs.length;
-  updateBadge(tabCount);
-});
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({url: "popup.html"});
-});
+chrome.tabs.query({ currentWindow: true }, handleTabCount);
