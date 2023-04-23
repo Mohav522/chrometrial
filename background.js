19de0b1b02f041c6cc5839c8359898527e9be3df
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-23 19:53:53 -0400

    v3 update

--- background.js
+++ background.js
@@ -1,38 +1,38 @@
 function updateBadge(tabCount) {
-  let color = "";
-  if (tabCount >= 1 && tabCount <= 5) {
-    color = "green";
-  } else if (tabCount >= 6 && tabCount <= 9) {
-    color = "yellow";
-  } else if (tabCount >= 10 && tabCount <= 10) {
-    color = "red";
-  }
-  const badgeText = tabCount > 9 ? "10" : tabCount.toString();
-  chrome.browserAction.setBadgeText({text: badgeText});
-  chrome.browserAction.setBadgeBackgroundColor({color: color});
-}
-
-chrome.tabs.onCreated.addListener(function(tab) {
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    const tabCount = tabs.length;
-    if (tabCount > 10) {
-      chrome.tabs.remove(tab.id);
-      updateBadge(tabCount);
-    } else {
-      updateBadge(tabCount);
+    let color = "";
+    if (tabCount >= 1 && tabCount <= 5) {
+      color = "green";
+    } else if (tabCount >= 6 && tabCount <= 9) {
+      color = "yellow";
+    } else if (tabCount >= 10 && tabCount <= 10) {
+      color = "red";
     }
+    const badgeText = tabCount > 9 ? "10" : tabCount.toString();
+    chrome.action.setBadgeText({text: badgeText});
+    chrome.action.setBadgeBackgroundColor({color: color});
+  }
+  
+  chrome.tabs.onCreated.addListener(function(tab) {
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+      const tabCount = tabs.length;
+      if (tabCount > 10) {
+        chrome.tabs.remove(tab.id);
+        updateBadge(tabCount);
+      } else {
+        updateBadge(tabCount);
+      }
+    });
   });
-});
-
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  
+  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+      const tabCount = tabs.length;
+      updateBadge(tabCount);
+    });
+  });
+  
   chrome.tabs.query({currentWindow: true}, function(tabs) {
     const tabCount = tabs.length;
     updateBadge(tabCount);
   });
-});
-
-chrome.tabs.query({currentWindow: true}, function(tabs) {
-  const tabCount = tabs.length;
-  updateBadge(tabCount);
-});
-
+  
