a925d0e089f5f351e1bfb40c69e101051161a263
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-23 14:00:42 -0400

    upd

---
+++ popup.js
@@ -0,0 +1,37 @@
+chrome.tabs.query({}, function(tabs) {
+  var count = tabs.length;
+
+  const MAX_TABS = 10;
+
+  chrome.tabs.onCreated.addListener(function(tab) {
+
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+
+      if (tabs.length > MAX_TABS) {
+
+        chrome.tabs.remove(tab.id);
+
+        chrome.notifications.create({
+          type: "basic",
+          title: "Tab Limit Exceeded",
+          message: "You have reached the maximum number of tabs in this window.",
+          iconUrl: "icon-48.png"
+        });
+      }
+    });
+  });
+
+  document.getElementById("Tab Limiter").textContent = count;
+});
+
+chrome.tabs.query({}, function(tabs) {
+    var count = tabs.length;
+    if (count >= 10 && count <= 11) {
+      var message = document.getElementById("message");
+      if (message) {
+        message.textContent = "Max limit reached!";
+      }
+    }
+    document.getElementById("Tab Limiter").textContent = count;
+  });
+  
