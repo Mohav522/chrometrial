755a1da6b9ea14f4247ef20c48219bbed1906315
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-23 14:15:27 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,26 +1,50 @@
 function updateBadge(tabCount) {
-    let color = "";
-    if (tabCount >= 1 && tabCount <= 5) {
-      color = "green";
-    } else if (tabCount >= 6 && tabCount <= 9) {
-      color = "yellow";
-    } else if (tabCount >= 10 && tabCount <= 10) {
-      color = "red";
-    }
-    const badgeText = tabCount > 9 ? "10" : tabCount.toString();
-    chrome.browserAction.setBadgeText({text: badgeText});
-    chrome.browserAction.setBadgeBackgroundColor({color: color});
+  let color = "";
+  if (tabCount >= 1 && tabCount <= 5) {
+    color = "green";
+  } else if (tabCount >= 6 && tabCount <= 9) {
+    color = "yellow";
+  } else if (tabCount >= 10 && tabCount <= 10) {
+    color = "red";
   }
-  
-  chrome.tabs.onCreated.addListener(function(tab) {
+  const badgeText = tabCount > 9 ? "10" : tabCount.toString();
+  chrome.browserAction.setBadgeText({text: badgeText});
+  chrome.browserAction.setBadgeBackgroundColor({color: color});
+}
+
+chrome.tabs.onCreated.addListener(function(tab) {
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    const tabCount = tabs.length;
+    if (tabCount > 10) {
+      chrome.tabs.remove(tab.id);
+      updateBadge(tabCount);
+    } else {
+      updateBadge(tabCount);
+    }
+  });
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    const tabCount = tabs.length;
+    updateBadge(tabCount);
+  });
+});
+
+chrome.tabs.query({currentWindow: true}, function(tabs) {
+  const tabCount = tabs.length;
+  updateBadge(tabCount);
+});
+
+chrome.tabs.onCreated.addListener(function(tab) {
     chrome.tabs.query({currentWindow: true}, function(tabs) {
       const tabCount = tabs.length;
-      if (tabCount >= 10) {
+      if (tabCount > 10) {
         chrome.tabs.remove(tab.id);
         chrome.notifications.create({
           type: "basic",
           title: "Tab Limit Exceeded",
-          message: "You have reached the maximum number of tabs allowed.",
+          message: "Stop right here, there won't be an 11th!",
           iconUrl: "icon.png"
         }, function() {
           setTimeout(function() {
@@ -31,17 +55,5 @@ function updateBadge(tabCount) {
         updateBadge(tabCount);
       }
     });
-  });
-  
-  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      const tabCount = tabs.length;
-      updateBadge(tabCount);
-    });
-  });
-  
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    const tabCount = tabs.length;
-    updateBadge(tabCount);
   });
   
