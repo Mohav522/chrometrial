2edf373e2b0554584e6579d94b2350def86e6947
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-23 14:17:51 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,47 +1,38 @@
 function updateBadge(tabCount) {
-    let color = "";
-    if (tabCount >= 1 && tabCount <= 5) {
-      color = "green";
-    } else if (tabCount >= 6 && tabCount <= 9) {
-      color = "yellow";
-    } else if (tabCount >= 10 && tabCount <= 10) {
-      color = "red";
-    }
-    const badgeText = tabCount > 9 ? "10" : tabCount.toString();
-    chrome.browserAction.setBadgeText({text: badgeText});
-    chrome.browserAction.setBadgeBackgroundColor({color: color});
+  let color = "";
+  if (tabCount >= 1 && tabCount <= 5) {
+    color = "green";
+  } else if (tabCount >= 6 && tabCount <= 9) {
+    color = "yellow";
+  } else if (tabCount >= 10 && tabCount <= 10) {
+    color = "red";
   }
-  
-  chrome.tabs.onCreated.addListener(function(tab) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      const tabCount = tabs.length;
-      if (tabCount > 9) {
-        chrome.tabs.remove(tab.id);
-        chrome.notifications.create({
-          type: "basic",
-          title: "Tab Limit Exceeded",
-          message: "You can't open more than 10 tabs at once!",
-          iconUrl: "icon.png"
-        }, function() {
-          setTimeout(function() {
-            chrome.notifications.clear("tab-limit-notification");
-          }, 3000);
-        });
-      } else {
-        updateBadge(tabCount);
-      }
-    });
-  });
-  
-  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      const tabCount = tabs.length;
+  const badgeText = tabCount > 9 ? "10" : tabCount.toString();
+  chrome.browserAction.setBadgeText({text: badgeText});
+  chrome.browserAction.setBadgeBackgroundColor({color: color});
+}
+
+chrome.tabs.onCreated.addListener(function(tab) {
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    const tabCount = tabs.length;
+    if (tabCount > 10) {
+      chrome.tabs.remove(tab.id);
       updateBadge(tabCount);
-    });
+    } else {
+      updateBadge(tabCount);
+    }
   });
-  
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
   chrome.tabs.query({currentWindow: true}, function(tabs) {
     const tabCount = tabs.length;
     updateBadge(tabCount);
   });
-  
+});
+
+chrome.tabs.query({currentWindow: true}, function(tabs) {
+  const tabCount = tabs.length;
+  updateBadge(tabCount);
+});
+
