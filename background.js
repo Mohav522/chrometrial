573fb0d4c0f2e5d58490c0e90fb7b7d72c3cf26e
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-23 02:37:21 -0400

    background.js update

--- background.js
+++ background.js
@@ -1,6 +1,20 @@
+var tabCount = 0;
+
+function updateBadge(tabCount) {
+  const badgeText = tabCount > 9 ? "10+" : tabCount.toString();
+  chrome.browserAction.setBadgeText({text: badgeText});
+}
+
+chrome.tabs.query({currentWindow: true}, function(tabs) {
+  tabCount = tabs.length;
+  updateBadge(tabCount);
+});
+
 chrome.tabs.onCreated.addListener(function(tab) {
   chrome.tabs.query({currentWindow: true}, function(tabs) {
-    if (tabs.length > 10) {
+    tabCount = tabs.length;
+    updateBadge(tabCount);
+    if (tabCount > 10) {
       chrome.tabs.remove(tab.id);
     }
   });
@@ -8,42 +22,7 @@ chrome.tabs.onCreated.addListener(function(tab) {
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
   chrome.tabs.query({currentWindow: true}, function(tabs) {
-    if (tabs.length > 10) {
-      chrome.browserAction.setBadgeText({text: "10+"});
-    } else {
-      chrome.browserAction.setBadgeText({text: ""});
-    }
+    tabCount = tabs.length;
+    updateBadge(tabCount);
   });
 });
-
-chrome.tabs.query({currentWindow: true}, function(tabs) {
-  if (tabs.length > 10) {
-    chrome.browserAction.setBadgeText({text: "10+"});
-  } else {
-    chrome.browserAction.setBadgeText({text: ""});
-  }
-});
-
-var tabCount = 0;
-
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-  tabCount--;
-});
-
-function updateBadge(tabCount) {
-    const badgeText = tabCount > 9 ? "10+" : tabCount.toString();
-    chrome.browserAction.setBadgeText({text: badgeText});
-  }
-  
-  chrome.tabs.onCreated.addListener(function(tab) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      const tabCount = tabs.length;
-      if (tabCount > 10) {
-        chrome.tabs.remove(tab.id);
-        updateBadge(tabCount);
-      } else {
-        updateBadge(tabCount);
-      }
-    });
-  });
-  
