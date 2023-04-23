17d069ba86e22315a9dc40627d5fc49c1a2d87bc
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-04-23 13:51:23 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,74 +1,44 @@
-function showNotification() {
-    chrome.notifications.create({
-      type: "basic",
-      title: "Tab Limit Exceeded",
-      message: "Stop right here, there won't be an 11th!",
-      iconUrl: "icon.png"
-    });
-  }
-  
-  chrome.tabs.onCreated.addListener(function(tab) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      const tabCount = tabs.length;
-      if (tabCount > 10) {
-        chrome.tabs.remove(tab.id);
-        showNotification();
-      }
-    });
-  });
-  
-  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      const tabCount = tabs.length;
-      if (tabCount > 10) {
-        showNotification();
-      }
-    });
-  });
-  
+var tabCount = 0;
+
+chrome.tabs.onCreated.addListener(function(tab) {
   chrome.tabs.query({currentWindow: true}, function(tabs) {
     const tabCount = tabs.length;
     if (tabCount > 10) {
-      showNotification();
-    }
-  });
-  
-  function updateBadge(tabCount) {
-    let color = "";
-    if (tabCount >= 1 && tabCount <= 5) {
-      color = "green";
-    } else if (tabCount >= 6 && tabCount <= 8) {
-      color = "yellow";
-    } else if (tabCount >= 9 && tabCount <= 10) {
-      color = "red";
-    }
-    const badgeText = tabCount > 9 ? "10+" : tabCount.toString();
-    chrome.browserAction.setBadgeText({text: badgeText});
-    chrome.browserAction.setBadgeBackgroundColor({color: color});
-  }
-  
-  chrome.tabs.onCreated.addListener(function(tab) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      const tabCount = tabs.length;
-      if (tabCount > 10) {
-        chrome.tabs.remove(tab.id);
-        updateBadge(tabCount);
-      } else {
-        updateBadge(tabCount);
-      }
-    });
-  });
-  
-  chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-      const tabCount = tabs.length;
+      chrome.tabs.remove(tab.id);
       updateBadge(tabCount);
-    });
+      showNotification();
+    } else {
+      updateBadge(tabCount);
+    }
   });
-  
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    const tabCount = tabs.length;
-    updateBadge(tabCount);
-  });
-  
-  
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  tabCount--;
+  updateBadge(tabCount);
+});
+
+function updateBadge(tabCount) {
+  let badgeColor = "";
+  if (tabCount >= 1 && tabCount <= 5) {
+    badgeColor = "#00FF00"; // green
+  } else if (tabCount >= 6 && tabCount <= 8) {
+    badgeColor = "#FFFF00"; // yellow
+  } else if (tabCount >= 9 && tabCount <= 10) {
+    badgeColor = "#FF0000"; // red
+  }
+  const badgeText = tabCount > 9 ? "10" : tabCount.toString();
+  chrome.browserAction.setBadgeText({text: badgeText});
+  chrome.browserAction.setBadgeBackgroundColor({color: badgeColor});
+}
+
+function showNotification() {
+  const message = "Stop right here, there won't be an 11th!";
+  const options = {
+    type: "basic",
+    title: "Tab Limit Reached",
+    message: message,
+    iconUrl: "icon.png"
+  };
+  chrome.notifications.create(options);
+}
