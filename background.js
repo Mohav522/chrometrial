27d7c59002daa18a00d466108672b36a4201ace6
Mohav522 <114080953+Mohav522@users.noreply.github.com>
2023-05-17 00:12:47 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,18 +1,20 @@
+const MAX_TAB_LIMIT = 10;
+
 function updateBadge(tabCount) {
   let color = "";
   if (tabCount >= 1 && tabCount <= 5) {
     color = "#00FF00";
   } else if (tabCount >= 6 && tabCount <= 9) {
     color = "#FFFF00";
-  } else if (tabCount >= 10 && tabCount <= 10) {
+  } else if (tabCount >= MAX_TAB_LIMIT) {
     color = "#FF0000";
   }
 
-  const badgeText = tabCount > 9 ? "10" : tabCount.toString();
+  const badgeText = tabCount > MAX_TAB_LIMIT ? `${MAX_TAB_LIMIT}+` : tabCount.toString();
   chrome.action.setBadgeText({ text: badgeText });
   chrome.action.setBadgeBackgroundColor({ color: color });
 
-  if (tabCount >= 10) {
+  if (tabCount >= MAX_TAB_LIMIT) {
     chrome.windows.getCurrent(function (currentWindow) {
       chrome.tabs.query({ active: true, windowId: currentWindow.id }, function (activeTabs) {
         const activeTab = activeTabs[0];
@@ -29,6 +31,9 @@ chrome.tabs.onCreated.addListener(function (tab) {
   chrome.tabs.query({ currentWindow: true }, function (tabs) {
     const tabCount = tabs.length;
     updateBadge(tabCount);
+    if (tabCount > MAX_TAB_LIMIT) {
+      chrome.tabs.remove(tab.id);
+    }
   });
 });
 
